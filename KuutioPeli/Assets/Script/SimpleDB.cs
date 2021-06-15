b1e62812f69d199045fb546be0ef673bb111ce64
DanielH1337 <62145497+DanielH1337@users.noreply.github.com>
2021-06-15 23:17:18 +0300

    SQLiten asennus ja yksinkertaisen tietokannan luonti

--- KuutioPeli/Assets/Script/SimpleDB.cs
+++ KuutioPeli/Assets/Script/SimpleDB.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Mono.Data.Sqlite;
+using System.Data;
+
+
 
 public class SimpleDB : MonoBehaviour
 {
@@ -13,6 +16,7 @@ public class SimpleDB : MonoBehaviour
         CreateDB();
         AddWeapon("Silver sword", 30);
         DisplayWeapons();
+        
     }
     //Metodi jossa luodaan tietokanta
     public void CreateDB()
@@ -20,17 +24,52 @@ public class SimpleDB : MonoBehaviour
         //Luodaan yhteys tietokantaan
         using (var connection = new SqliteConnection(dbName))
         {
+            connection.Open();
             using (var command = connection.CreateCommand())
             {
                 command.CommandText = "CREATE TABLE IF NOT EXISTS weapons (name VARCHAR(20), damage INT);";
                 command.ExecuteNonQuery();
             }
+            connection.Close();
         } 
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AddWeapon(string weaponName, int weaponDamage)
     {
-        
+        using (var connection = new SqliteConnection(dbName))
+        {
+            connection.Open();
+
+            using(var command = connection.CreateCommand())
+            {
+                command.CommandText = "INSERT INTO weapons (name, damage) VALUES ('" + weaponName + "', '" + weaponDamage + "');";
+                command.ExecuteNonQuery();
+            }
+            connection.Close();
+        }
     }
+
+    public void DisplayWeapons()
+    {
+        using (var connection = new SqliteConnection(dbName))
+        {
+            connection.Open();
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = "SELECT * FROM weapons;";
+                using (IDataReader reader = command.ExecuteReader())
+                {
+                    
+                    while (reader.Read())
+                    
+                        Debug.Log("Name: " + reader["name"] + "\tDamage: " + reader["damage"]);
+                    
+                    reader.Close();
+                }
+            }
+            connection.Close();
+        }
+    }
+
+ 
 }
