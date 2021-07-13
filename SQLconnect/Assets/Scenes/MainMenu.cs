342e76d1bafe3fc66f658179e9940c73738f724e
Markus Anttila <42510680+BlackMorphine@users.noreply.github.com>
2021-07-13 11:38:41 +0300

    Inventory scriptit
    Inventory system scriptit (diablo like)

---
+++ SQLconnect/Assets/Scenes/MainMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+üsing UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour{
+    public void GoToRegister()
+    {
+        SceneManager.LoadScene(1);
+
+    }
+}
