474f038a53bd162fb9ae47a85ac91c2eb41b70f2
DanielH1337 <62145497+DanielH1337@users.noreply.github.com>
2021-09-20 13:59:33 +0300

    Inventaario Scriptien aloitus

--- KuutioPeli/Assets/Script/Inventory/SC_InventorySystem.cs
+++ KuutioPeli/Assets/Script/Inventory/SC_InventorySystem.cs
@@ -7,7 +7,7 @@ public class SC_InventorySystem : MonoBehaviour
 
     public Texture crosshairTexture;
     public SC_CharacterController playerController;
-    public SC_Pickitem[] availableItems; //Prefab list
+    public SC_PickItem[] availableItems; //Prefab list
 
     //Free slots
 
@@ -24,7 +24,7 @@ public class SC_InventorySystem : MonoBehaviour
 
 
     //Pickup
-    SC_Pickitem detectedItem;
+    SC_PickItem detectedItem;
     int detectedItemIndex;
     
 
@@ -141,9 +141,9 @@ public class SC_InventorySystem : MonoBehaviour
 
             if (objectHit.CompareTag("Respawn"))
             {
-                if ((detectedItem == null || detectedItem.transform != objectHit) && objectHit.GetComponent<SC_Pickitem>() != null)
+                if ((detectedItem == null || detectedItem.transform != objectHit) && objectHit.GetComponent<SC_PickItem>() != null)
                 {
-                    SC_Pickitem itemTmp = objectHit.GetComponent<SC_Pickitem>();
+                    SC_PickItem itemTmp = objectHit.GetComponent<SC_PickItem>();
 
                     for (int i = 0; i < availableItems.Length; i++)
                     {
--- KuutioPeli/Assets/Script/Inventory/SC_PickItem.cs
+++ KuutioPeli/Assets/Script/Inventory/SC_PickItem.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SC_Pickitem : MonoBehaviour
+public class SC_PickItem : MonoBehaviour
 {
-    public string itemName = "Some Item"; //Unique name for all items
+    public string itemName = "Cube"; //Unique name for all items
     public Texture itemPreview;
 
     void Start()
